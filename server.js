1a785065a1548c840cdb3bbc3fb988a25c6233f3
codingwithsach <sajjad.ahmedali@gmail.com>
2024-06-25 19:51:17 +0500

    Test the error handling by accessing a non-existent route

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/user/:id', (req, res) => {
     res.send(`user ID: ${userId}`);
 });
 
+app.use((req, res, next) => {
+    res.status(404).send('404 not found');
+});
+
+app.use((err, res, req, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
