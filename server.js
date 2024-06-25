a20dd4e86181fd5fb435057d901ee1563c4e53eb
codingwithsach <sajjad.ahmedali@gmail.com>
2024-06-25 15:23:11 +0500

    Use json data like key value pair

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express")
-
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Node express!')
-});
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send('Node express!')
+// });
+
+app.post('/api/data', (req, res) => {
+    const { name, age } = req.body;
+    res.json({ message: `Received data: ${name}, ${age}`});
+})
 
 
 const PORT = process.env.PORT || 3000;
